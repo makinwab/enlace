338e32d3f78d6ff7ecbd6dcfc718d8a55feed5d4
Makinwa Olubukola <makinwa37@gmail.com>
2016-02-07 15:00:17 +0100

    Revert "Develop"

--- db/migrate/20160119133051_add_user_to_links.rb
+++
@@ -1,7 +0,0 @@
-class AddUserToLinks < ActiveRecord::Migration
-  def change
-    add_column :links, :user_id, :integer
-
-    add_index :links, :user_id
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-User.destroy_all
-Link.destroy_all
-
-User.create(
-  name: "Makinwa Bukola",
-  email: "makinwa37@gmail.com",
-  password: "makinwa37"
-)
-
-Link.create(
-  user_id: User.first.id,
-  given_url: "https://docs.google.com/document/d/1wVwa93VKX3XN9D-kwsf_aSNjWr2UTbfNn5YqBHuLpEU/edit",
-  slug: "5yuvtc",
-  clicks: 0,
-  snapshot: nil,
-  title: nil
-)
-
-
-puts "Seeding successful"

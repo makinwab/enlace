c0e1846593ee50e3ecef2b095b80a2c8643e840c
andela-bmakinwa <makinwa37@gmail.com>
2016-03-22 16:05:15 +0100

    Write tests for user controller and create factory data

--- spec/factories/user/user.rb
+++ spec/factories/user/user.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    name "makinwa bukola"
     email "makinwa37@gmail.com"
     password "makinwa37"
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,19 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
+  context "name" do
+    it { should validate_presence_of :name }
+  end
+  
+  context "email" do
+    it { should validate_presence_of :email }
+    it { should allow_value("email@address.com").for(:email) }
+    it { should_not allow_value("com").for(:email) }
+    it { should_not allow_value("email@address").for(:email) }
+    it { should_not allow_value("@.com").for(:email) }
+  end
+
+  context "password" do
+    it { should validate_presence_of :password }
+  end
 end
